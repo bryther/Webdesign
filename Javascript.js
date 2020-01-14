386f9a19730b78b9dab34df03b7aa638712de038
bryther <57659326+bryther@users.noreply.github.com>
2020-01-14 09:32:06 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+var counter = 0;
+
+function count() {
+    counter++;
+    document.getElementById('score').innerHTML = "score: " + counter;
+}
+
+function
+switch () {
+    document.getElementsById('ball').animation = "jump";
+}
